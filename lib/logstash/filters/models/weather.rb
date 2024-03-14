bfb5a85260340214ee149645d59e9450e273de5a
Miguel Alvarez <malvarez@redborder.com>
2024-03-14 15:19:03 +0000

    Add snow

--- lib/logstash/filters/models/weather.rb
+++ lib/logstash/filters/models/weather.rb
@@ -4,13 +4,14 @@
 # Model of the weather
 #
 class Weather
-  def initialize(location, temperature, metadata, wind_speed = 0, rain_1h = 0)
+  def initialize(location, temperature, metadata, wind_speed = 0, rain_1h = 0, snow_1h = 0)
     @location = location
     @temperature = temperature
     @weather_icon = metadata[:weather_icon]
     @weather_description = metadata[:weather_description]
     @wind_speed = wind_speed
     @rain_1h = rain_1h
+    @snow_1h = snow_1h
   end
 
   def to_hash
@@ -20,7 +21,8 @@ class Weather
       'weather_icon' => @weather_icon,
       'weather_description' => @weather_description,
       'wind_speed' => @wind_speed,
-      'rain_1h' => @rain_1h
+      'rain_1h' => @rain_1h,
+      'snow_1h' => @snow_1h
     }
   end
 end
